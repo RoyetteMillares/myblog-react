3d58f74c6c699a11bc2fdd572199b1f0d54a319a
Royette B. Millares <70820776+RoyetteMillares@users.noreply.github.com>
2023-01-08 21:18:24 +0800

    add login form not finished

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div className='flex-col px-10 py-5 bg-slate-400'>
-            <h1 className='font-bold text-lg'>Login</h1>
-            <form className='flex-col items-center'>
-                <input type="text" placeholder='username' />
-                <input type="password" placeholder='password' />
+        <div className='w-full'>
+            <h1 className='font-bold text-lg text-center mb-4'>Login</h1>
+            <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                    <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                </div>
             </form>
         </div>
     )
